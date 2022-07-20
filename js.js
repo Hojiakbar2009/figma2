158b8af7b339f964646076e59c937cab0d2c9620
codding <hojiabarnurullaev007@gmail.com>
2022-07-20 18:46:07 +0500

    the

--- js.js
+++ js.js
@@ -1,25 +1,100 @@
 
+// function data() {
+//        let data = {
+//         id: 1,
+//         ism: salom,
+//         model:1
+//        }
 
-let shop = {
-    name: "api",
-    age:"44"
 
+// }   
+
+let modal = document.querySelector(".modal"),
+    p_1 = document.querySelector(".p_1"),
+    btn = document.querySelector(".nav2_box");
+
+btn.addEventListener("click", function(){
+    modal.style.display = "block";
+});
+
+p_1.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+
+
+const btn_2 = document.querySelector(".btn"),
+    div_1 = document.querySelector(".div_1"),
+    div_2 = document.querySelector(".div_2"),
+    div_3 = document.querySelector(".div_3"),
+    div_4 = document.querySelector(".div_4"),
+    div_5 = document.querySelector(".div_5");  
+
+
+let section_1 = document.querySelector(".section_1");
+
+const data = [
+{
+    id:"6% OFF",
+    narxi:"Buy More & Save More",
+    nomi:"s",
+    soni:"Beverage",
+    
 }
 
+]
+data.map((item)=>{
+    section_1.innerHTML +=`
 
+    <div class="sec1_1">
+    <img src="${item.img}" alt="" >
 
-function bosing(){
-    localStorage.setItem("api", JSON.stringify(shop))
-
-}
-
-let loca = localStorage.getItem("api");
-
-loca.map((item)=>{
-    loca.innerHTML += `
-    <div class="item">
-    <p>${item.age}</p>
+    <p> ${item.id} </p>
+    <p> ${item.narxi} </p>
+    <p> ${item.nomi} </p>
+    <p> ${item.soni} </p>
+    <button class="ilyosbek"> sotib olish </button>
     </div>
-    `
-})
+    `;
+});
+
+let btn_3 = document.querySelector(".ilyosbek")
+
+btn_3.addEventListener("click", function(){
+  let Json = localStorage.getItem("salom", );
+  Json = JSON.parse(Json);
+    localStorage.setItem("salom", JSON.stringify(data));
+    Json.map((item)=> {
+        modal.innerHTML +=  `
+        <div class="div_1">
+        <div class="left">
+            <div class="div1">
+                <span>6% OFF</span>
+                <b>Buy More & Save More</b>
+                <p class="salom">Beverage</p>
+            </div>
+        </div>
+        
+        `;
+        
+        p_1.addEventListener("click", function () {
+            modal.style.display = "none";
+        });
+    })
+});
+
+
+
+
+
+
+// for (let k = 0; k <= 5; k++) {
+//     console.log(k)
+    
+// }
+
+
+
+
+
 
